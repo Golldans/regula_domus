41af966c70b8f4ff94ab513e728f41dbd75847a1
Cristiano Henrique Ferreira <13822400661@MUZAMBINHO.EDU.BR>
2025-12-09 21:47:47 -0300

    fix: definindo for feature do typeorm para todos os modulos - develop

--- regula_domus_backend/src/billings/billings.module.ts
+++ regula_domus_backend/src/billings/billings.module.ts
@@ -2,11 +2,15 @@ import { Module } from "@nestjs/common";
 import { BillingController } from "./presentation/billings.controller";
 import { BillingService } from "./domain/billings.service";
 import { BillingsRepository } from "./infra/repository/billings.repository";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BillingSchema } from "./infra/schema/billings.schema";
 
 @Module({
     controllers: [BillingController],
     exports: [BillingService],
-    imports: [],
+    imports: [TypeOrmModule.forFeature([
+        BillingSchema,
+    ])],
     providers: [BillingService, BillingsRepository]
 })
 export class BillingModule {}
--- regula_domus_backend/src/user/user.module.ts
+++ regula_domus_backend/src/user/user.module.ts
@@ -2,11 +2,15 @@ import { Module } from "@nestjs/common";
 import { UserController } from "./presentation/user.controller";
 import { UserService } from "./domain/user.service";
 import { UserRepository } from "./infra/repository/user.repository";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserSchema } from "./infra/schema/user.schema";
 
 @Module({
     controllers: [UserController],
     exports: [UserService],
-    imports: [],
+    imports: [TypeOrmModule.forFeature([
+        UserSchema,
+    ])],
     providers: [UserService, UserRepository]
 })
 export class UserModule {}
